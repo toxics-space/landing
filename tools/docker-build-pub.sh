9b0ad049a33604ce8fa771b2b01e059ee5e9b7ed
Mark Martynov <m.martynov@1inch.io>
2022-12-20 02:38:53 +0400

    fix: ci

--- tools/docker-build-pub.sh
+++ tools/docker-build-pub.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Dont forget to login
-docker build --build-arg TARGETPLATFORM=amd64 . -t ui:latest
+docker build --build-arg TARGETPLATFORM=amd64 . -t landing:latest
 docker tag landing:latest ghcr.io/toxics-space/landing:latest
 docker image push ghcr.io/toxics-space/landing:latest
