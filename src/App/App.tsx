efc6f6122552e1c98317c10a09f775bf16ea02ab
Mark Martynov <m.martynov@1inch.io>
2022-12-20 02:36:54 +0400

    initial

---
+++ src/App/App.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+import styles from './App.module.css';
+ 
+export function App () {
+
+  return <Box className={styles.app}>
+    <Box> Hello world! </Box>
+  </Box>
+}
