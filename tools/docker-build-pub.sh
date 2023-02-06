1fc69f55b77c73d3c8e9122ae0566f6d1028ce89
Mark Martynov <m.martynov@1inch.io>
2023-02-06 19:10:30 +0400

    chore: change registry

--- tools/docker-build-pub.sh
+++ tools/docker-build-pub.sh
@@ -2,5 +2,9 @@
 
 # Dont forget to login
 docker build --build-arg TARGETPLATFORM=amd64 -t landing:latest . 
-docker tag landing:latest ghcr.io/toxics-space/landing:latest
-docker image push ghcr.io/toxics-space/landing:latest
+
+# docker tag landing:latest ghcr.io/toxics-space/landing:latest
+# docker image push ghcr.io/toxics-space/landing:latest
+
+docker tag ui:latest reg.toxics.space/ui:latest
+docker image push reg.toxics.space/ui:latest
